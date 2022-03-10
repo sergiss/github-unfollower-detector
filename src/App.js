2c89bff7df96da38cd2102b96972fbd6a09319d2
sergi <sergi.ss4@gmail.com>
2022-03-10 20:00:55 +0100

    comment log output

--- src/App.js
+++ src/App.js
@@ -129,6 +129,7 @@ function App() {
     let keys = Object.keys(data.following);
     for(let i = 0; i < keys.length; ++i) {
       if(!data.followers[keys[i]]) {
+        // console.log(keys[i])
         notMutual.push(data.following[keys[i]]);
       }
     }
